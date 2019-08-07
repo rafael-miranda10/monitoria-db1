d00b6085e2c7f1b3613375aa08ab40d95ac62bb5
rafael-miranda10 <arthur.rafa10@gmail.com>
2019-08-07 20:29:42 -0300

    [DEV] Somente criação do projeto de testes para os services

---
+++ MonitoriaDB1/Monitoria.Services.PetCare.Tests/Services/RowAnimalServicesTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monitoria.Services.PetCare.Tests.Services
+{
+    [TestClass]
+    public class RowAnimalServicesTests
+    {
+        //https://www.lambda3.com.br/2012/06/testes-integrados-com-webapi/
+        //https://flaviohenriquedecarvalho.wordpress.com/2015/02/18/o-que-onde-quando-e-quem-testar-no-seu-codigo/
+    }
+}
